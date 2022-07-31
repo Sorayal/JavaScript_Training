a6bc1d40beef26a31e4f1b535b2fbc46a5f40329
Sascha Soerries <32453878+Sorayal@users.noreply.github.com>
2022-07-31 12:31:18 +0200

    added one file for arithmetic calculation

---
+++ JetBrains_Excercises/ArithmeticOperators/fifteen.js
@@ -0,0 +1,5 @@
+
+console.log(51 % 18);
+console.log(3 * 5);
+console.log(76 / 5); //this is different to Java where it keeps it as Integer when only Integer are given
+console.log((51 / 18) * 2); // same as above comment
